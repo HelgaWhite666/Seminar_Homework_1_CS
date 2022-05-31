156c94938e601a981546e509ff56d3f1f4ca6df5
Helga <olyapepsycola@gmail.com>
2022-05-31 22:41:18 +0300

    Eercise_007

---
+++ Exercise_007/Program.cs
@@ -0,0 +1,23 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+int number = new Random().Next(100,999);
+Console.Write ("Дано число: ");
+Console.WriteLine (number);
+
+int a = number / 100;
+Console.Write ("Первая цифра заданного числа: ");
+Console.WriteLine (a);
+
+int b = (number % 100) / 10;
+Console.Write ("Вторая цифра заданного числа: ");
+Console.WriteLine (b);
+
+int c = number % 10;
+Console.Write ("Последняя цифра заданного числа: ");
+Console.WriteLine (c);
+
+Console.Write ("Число без второй цифры: ");
+Console.Write (a);
+Console.WriteLine (c);
+
+
