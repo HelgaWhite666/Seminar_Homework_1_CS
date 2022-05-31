53d05a7f722a9708036aead9accbf6a2c4ed9f1c
Helga <olyapepsycola@gmail.com>
2022-05-31 22:21:28 +0300

    Exercise_005

---
+++ Exercise_005/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int a = new Random().Next(100,999);
+Console.Write ("Дано число: ");
+Console.WriteLine (a);
+
+Console.Write ("Вторая цифра заданного числа: ");
+Console.WriteLine ((a % 100) / 10);
