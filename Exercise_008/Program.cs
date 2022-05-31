cd7b81088f208d374a04dbb91366f070330fb10c
Helga <olyapepsycola@gmail.com>
2022-05-31 23:13:04 +0300

    Exercise_008

---
+++ Exercise_008/Program.cs
@@ -0,0 +1,26 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток
+
+int number = new Random().Next(10,999);
+Console.Write ("Дано число: ");
+Console.WriteLine (number);
+
+Console.Write ("Введите любое число от 2 до 9: ");
+int a = int.Parse (Console.ReadLine());
+
+if (number % a == 0) {
+
+    Console.Write ("Число ");
+    Console.Write (number);
+    Console.Write (" кратно числу ");
+    Console.WriteLine (a);
+
+} else {
+
+    Console.Write ("Остаток от деления числа ");
+    Console.Write (number);
+    Console.Write (" на ");
+    Console.Write (a);
+    Console.Write (": ");
+    Console.WriteLine (number % a);
+
+}
