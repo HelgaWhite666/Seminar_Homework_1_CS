61f2346d497143791fa73df2beba08d74d8e7e6c
Helga <olyapepsycola@gmail.com>
2022-05-31 21:55:48 +0300

    Exercise_002

--- Exercise_001/Program.cs
+++ Exercise_001/Program.cs
@@ -1,11 +1,13 @@
-﻿Console.WriteLine("Введите число от 1 до 7 включительно:");
-int dayNumber = int.Parse(Console.ReadLine());
+﻿// По заданному номеру дня недели вывести его название
 
-if (dayNumber == 1) Console.WriteLine("Первый день недели - понедельник.");
-else if (dayNumber == 2) Console.WriteLine("Второй день недели - вторник.");
-else if (dayNumber == 3) Console.WriteLine("Третий день недели - среда.");
-else if (dayNumber == 4) Console.WriteLine("Четвертый день недели - четверг.");
-else if (dayNumber == 5) Console.WriteLine("Пятый день недели - пятница.");
-else if (dayNumber == 6) Console.WriteLine("Шестой день недели - суббота.");
-else if (dayNumber == 7) Console.WriteLine("Седьмой день недели - воскресенье.");
-else Console.WriteLine("Заданное число не соответствует какому-либо дню недели!");
+Console.Write ("Введите число от 1 до 7 включительно:");
+int dayNumber = int.Parse (Console.ReadLine());
+
+if (dayNumber == 1) Console.WriteLine ("Первый день недели - понедельник.");
+else if (dayNumber == 2) Console.WriteLine ("Второй день недели - вторник.");
+else if (dayNumber == 3) Console.WriteLine ("Третий день недели - среда.");
+else if (dayNumber == 4) Console.WriteLine ("Четвертый день недели - четверг.");
+else if (dayNumber == 5) Console.WriteLine ("Пятый день недели - пятница.");
+else if (dayNumber == 6) Console.WriteLine ("Шестой день недели - суббота.");
+else if (dayNumber == 7) Console.WriteLine ("Седьмой день недели - воскресенье.");
+else Console.WriteLine ("Заданное число не соответствует какому-либо дню недели!");
---
+++ Exercise_002/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти максимальное из трех чисел
+
+Console.Write ("Введите число 1: ");
+int a = int.Parse (Console.ReadLine());
+
+Console.Write ("Введите число 2: ");
+int b = int.Parse (Console.ReadLine());
+
+Console.Write ("Введите число 3: ");
+int c = int.Parse (Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write ("Максимальное число = ");
+Console.WriteLine (max);
