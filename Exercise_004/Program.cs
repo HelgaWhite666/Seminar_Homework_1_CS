d55f3335f01b137e5c11f29f720e099d7e611d91
Helga <olyapepsycola@gmail.com>
2022-05-31 22:17:02 +0300

    Exercise_004

---
+++ Exercise_004/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+double a = new Random().Next(100,999);
+Console.Write ("Дано число: ");
+Console.WriteLine (a);
+
+Console.Write ("Последняя цифра заданного числа: ");
+Console.WriteLine (a % 10);
