0fa3da304cb0eaf2394b07aeebbe0c0a1c3ffc10
Helga <olyapepsycola@gmail.com>
2022-05-31 22:30:31 +0300

    Exercise_006

---
+++ Exercise_006/Program.cs
@@ -0,0 +1,14 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random().Next(10,100);
+Console.Write ("Дано число: ");
+Console.WriteLine (number);
+
+int a = number / 10;
+Console.Write ("Первая цифра заданного числа: ");
+Console.WriteLine (a);
+
+int b = number % 10;
+Console.Write ("Вторая цифра заданного числа: ");
+Console.WriteLine (b);
+
